d7caaebfbd392d9fa025162f98a8ce5446c7c790
Jaryllim98 <34784727+Jaryllim98@users.noreply.github.com>
2020-04-11 17:42:08 +0800

    almost done with the deletion workflow
    added intentMaps to the releveant dialogflow intents, defined the deletion functions. left with extracting google calendar event ID for specific deletion events. (this beats me). if this is done the deletion workflow willbe complete

--- main.py
+++ main.py
@@ -16,6 +16,53 @@ import json
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+    service.events().delete(calendarId='primary', eventId='0o62hu9kavqrufb5ebeh3t29sl').execute()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+'''
 def test(request):
     result = intentMap.execute_intent(request.json)
     print('hello this is me')
@@ -51,12 +98,47 @@ def intentTest(request):
         start = event['start'].get('dateTime', event['start'].get('date'))
         testlist.append('{} {} {} \n'.format(index, start, event['summary']))
         print(index, start, event['summary'], 'hello i have succeeded')
-    return {'fulfillmentText': ' '.join(testlist)}
+    return {'fulfillmentText': ' '.join(testlist), 'fulfillmentlist': testlist}
 
 def delete1(request):
     print(request)
-    return {'fulfillmentText':'this is a test'}
-
+    idx = int(request["queryResult"]["queryText"])
+    print(idx)
+    print('jaryl is a smart person and he knows it too')
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    service = build('calendar', 'v3', credentials=creds)
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                          maxResults=10, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    if not events:
+        print('No upcoming events found.')
+    testlist = ['Are you sure you want to delete the following event?']
+    for index, event in enumerate(events):
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        testlist.append('{} {} {} \n'.format(index, start, event['summary']))
+    print(testlist)
+    return {'fulfillmentText': 'Are you sure you want to delete the following event {}'.format(testlist[idx])}
+def delete2(request):
+    if request['queryResult']["queryText"] == 'YES':
+        #run deletion code 
+        #service.events().delete(calendarId='primary', eventId='0o62hu9kavqrufb5ebeh3t29sl').execute()
+    else:
+        return {'fulfillmentText': 'Your booking has not been removed. Please try again.'}
+    return {'fulfillmentText':'Your booking has been removed. Please make another booking if you wish to'}
 def schedule1(request):
     return {'fulfillmentText': 'Please input your start date/time according to the format listed: (DD/MM/YY TIME 01/03/2020 1600)'}
 def schedule2(request):
@@ -127,14 +209,13 @@ def schedule4(request):
 intentMap = IntentMap()
 intentMap.add('Delete Calender Event - Choose List(1)', intentTest)
 intentMap.add('Delete Calender Event - Check(2)',delete1)
-
-
+intentMap.add('Delete Calender Event - Confirmed', delete2)
 intentMap.add('Book Slot - Start Time (1)', schedule1)
 intentMap.add('Book Slot - End Time (2)', schedule2)
 intentMap.add('Book Slot Confirmation (3)',schedule3)
 intentMap.add('BookSlot - Confirmed (4)', schedule4)
 
 #teleuser = request_json['originalDetectIntentRequest']['payload']['data']['from']['first_name']
-#teleid = request_json['originalDetectIntentRequest']['payload']['data']['from']['id']
+#teleid = request_json['originalDetectIntentRequest']['payload']['data']['from']['id']'''
 
     
